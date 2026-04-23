2e826f0305d394a31bb38d2cb2575fc64c8792c2
Abdul-LJ <stgaabor@ljmu.ac.uk>
2026-04-23 17:16:47 +0300

    Add files via upload

---
+++ graph/my_project-workshop/top_6_games.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_excel("dataset will be used.xlsx")
+
+data.columns = ["year", "game_name", "id", "searches"]
+data = data.iloc[1:]
+data["searches"] = data["searches"].astype(int)
+
+# groups the data by game name and adds together all search values for each gam
+total_searches = data.groupby("game_name")["searches"].sum()
+
+# sorts the games from highest to lowest searches and selects the top six.
+top_6 = total_searches.sort_values(ascending=False).head(6)
+
+# chart size
+plt.figure(figsize=(10,5))
+
+# bar chart
+top_6.plot(kind="bar")
+
+plt.xlabel("Game Name")
+plt.ylabel("Total Searches")
+plt.title("Top 6 Most Searched Games")
+
+#rotaion for the lines
+plt.xticks(rotation=45)
+
+# horizental lines
+plt.grid(axis='y')
+plt.tight_layout()
+
+plt.show()
